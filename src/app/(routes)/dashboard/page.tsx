1001174258af5a636a706380891db99878854098
giojano1 <giorgi.janelidze64@gmail.com>
2025-12-16 17:13:12 +0400

    cealnup-unused

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import Preview from "@/components/common/preview/preview";
 import Profile from "@/components/common/profile/profile";
 import { useRequiredUser } from "@/hooks/query/use-current-user";
 
 export default function DashboardPage() {
-  const user = useRequiredUser();
   return (
     <section className="w-full flex justify-between">
       <Profile />
