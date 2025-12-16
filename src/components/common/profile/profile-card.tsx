4d90f4e94b919dc2bc67ff65e4880204f9f7f6ed
giojano1 <giorgi.janelidze64@gmail.com>
2025-12-16 13:27:42 +0400

    feat/section-card-fold

--- src/components/common/profile/profile-card.tsx
+++ src/components/common/profile/profile-card.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
+import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
+import React, { useState } from "react";
 
 export default function ProfileCard({
   children,
@@ -8,12 +12,26 @@ export default function ProfileCard({
   children: React.ReactNode;
   title: string;
 }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <Card className="w-full bg-muted">
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
+      <CardHeader className="h-9.5 ">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-xl font-semibold">{title}</CardTitle>
+          <Button size="icon" variant="outline" onClick={handleToggle}>
+            <ChevronDown
+              className={cn(
+                isOpen ? "rotate-180" : "rotate-0",
+                "transition-transform"
+              )}
+            />
+          </Button>
+        </div>
       </CardHeader>
-      <CardContent>{children}</CardContent>
+      {isOpen && <CardContent>{children}</CardContent>}
     </Card>
   );
 }
