11bde15e969fd2d7b183c84910841a819a3a3bc2
giojano1 <giorgi.janelidze64@gmail.com>
2025-12-16 13:19:29 +0400

    feat/personal-details-form

--- src/components/common/profile/cards/personal-details.tsx
+++ src/components/common/profile/cards/personal-details.tsx
@@ -3,25 +3,47 @@ import FormInput from "@/components/ui/form-input";
 import FormTextarea from "@/components/ui/form-textarea";
 import ProfileCard from "../profile-card";
 import FormWrapper from "@/components/ui/form-wrapper";
+import { useTypedForm } from "@/hooks/query/use-typed-form";
+import { defaultProfileValues, profileSchema } from "@/schemas/profile.schema";
 
 export default function PersonalDetailsCard() {
-  const handleSubmit = (data: any) => {};
+  const formMethods = useTypedForm(profileSchema, {
+    defaultValues: defaultProfileValues,
+  });
+  const { register, formState } = formMethods;
+
+  const handleSubmit = (data: any) => {
+    console.log(handleSubmit);
+  };
+  const isPending = false;
   return (
     <ProfileCard title="Personal Details">
       <FormWrapper
         onSubmit={handleSubmit}
-        formMethods={{} as any}
+        formMethods={formMethods}
         ariaLabel="Personal Details Form"
         submitButtonLabel="Save"
         submitButtonLoadingLabel="Saving..."
         isPending={false}
       >
         <div className="flex items-center gap-4">
-          <FormInput placeholder="John Doe" label="Full Name" name="fullName" />
+          <FormInput
+            placeholder="John Doe"
+            label="Full Name"
+            name="fullName"
+            register={register}
+            errors={formState.errors}
+            disabled={isPending}
+            required
+          />
           <FormInput
             placeholder="Software Engineer"
             label="Title"
             name="title"
+            register={register}
+            errors={formState.errors}
+            disabled={isPending}
+            required
           />
         </div>
         <div>
@@ -30,11 +52,28 @@ export default function PersonalDetailsCard() {
             placeholder="A short bio about yourself"
             name="bio"
             className="resize-none h-25 bg-white"
+            register={register}
+            errors={formState.errors}
+            disabled={isPending}
           />
         </div>
         <div className="flex items-center gap-4">
-          <FormInput placeholder="" label="Location" name="location" />
-          <FormInput placeholder="" label="Website" name="website" />
+          <FormInput
+            placeholder=""
+            label="Location"
+            name="location"
+            register={register}
+            errors={formState.errors}
+            disabled={isPending}
+          />
+          <FormInput
+            placeholder=""
+            label="Website"
+            name="website"
+            register={register}
+            errors={formState.errors}
+            disabled={isPending}
+          />
         </div>
       </FormWrapper>
     </ProfileCard>
--- src/schemas/profile.schema.ts
+++ src/schemas/profile.schema.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 export const profileSchema = z.object({
   fullName: z
     .string()
-    .min(2, "Full name must be at least 2 characters")
+    .min(1, "Full name is required")
     .max(100, "Full name must be at most 100 characters")
     .trim(),
   title: z
     .string()
-    .min(2, "Title must be at least 2 characters")
+    .min(1, "Title is required")
     .max(100, "Title must be at most 100 characters")
     .trim(),
   bio: z
